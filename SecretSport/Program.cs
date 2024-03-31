7de30ef743fe3a023456969f44dbdda3e2b57780
Luis Torres <luis.torres.dev@outlook.com>
2024-03-31 15:28:06 -0500

    Solve the problem

---
+++ SecretSport/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SecretSport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var testCases = int.Parse(Console.ReadLine());
+
+            while (testCases-- > 0)
+            {
+                var totalPlays = int.Parse(Console.ReadLine());
+                var plays = Console.ReadLine();
+                var result = plays.Last() is 'A' ? 'A' : 'B';
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
